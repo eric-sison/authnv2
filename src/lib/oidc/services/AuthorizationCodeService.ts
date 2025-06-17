e3122ce82fec6febe1615f5593b198ed9b5bdb97
eric-sison <ericsison.dev@gmail.com>
2025-06-17 16:25:47 +0800

    replaced generateCuid() with a 3rd party package

--- src/lib/oidc/services/AuthorizationCodeService.ts
+++ src/lib/oidc/services/AuthorizationCodeService.ts
@@ -1,11 +1,11 @@
-import { generateCuid } from "@/utils/generateCuid";
 import { AuthorizationCode, AuthorizationCodePayload } from "../types/oidc";
+import { createId } from "@paralleldrive/cuid2";
 
 export class AuthorizationCodeService {
   private readonly codeExpirationTime: number = 600; // 10 minutes
 
   public generateAuthCode(payload: AuthorizationCodePayload, expiration?: number) {
-    const code = generateCuid();
+    const code = createId();
     const expiresAt = expiration
       ? new Date(Date.now() + expiration * 1000)
       : new Date(Date.now() + this.codeExpirationTime * 1000);
