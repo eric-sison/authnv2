dda06a7e1903488adba59deb424407dd7fa7098a
eric-sison <ericsison.dev@gmail.com>
2025-07-01 14:41:15 +0800

    removed redundant cuid library

--- src/lib/oidc/services/AuthorizationCodeService.ts
+++ src/lib/oidc/services/AuthorizationCodeService.ts
@@ -1,6 +1,6 @@
 //import { AuthorizationCodeRepository } from "../repositories/AuthorizationCodeRepository";
 import { AuthorizationCode, AuthorizationCodePayload } from "../types/oidc";
-import { createId } from "@paralleldrive/cuid2";
+import cuid from "cuid";
 
 export class AuthorizationCodeService {
   // constructor(private readonly authCodeRepository: AuthorizationCodeRepository) {}
@@ -8,7 +8,7 @@ export class AuthorizationCodeService {
   private readonly codeExpirationTime: number = 600; // 10 minutes
 
   public generateAuthCode(payload: AuthorizationCodePayload, expiration?: number) {
-    const code = createId();
+    const code = cuid();
     const expiresAt = expiration
       ? new Date(Date.now() + expiration * 1000)
       : new Date(Date.now() + this.codeExpirationTime * 1000);
